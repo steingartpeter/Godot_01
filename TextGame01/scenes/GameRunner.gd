4e115a26bb9bdde54ae95e0c536c13ee1258c1b1
steingartpeter <steingart.p2@gmail.com>
2023-04-25 20:46:13 +0200

    2024-03-25 005 smartgit

--- TextGame01/scenes/GameRunner.gd
+++ TextGame01/scenes/GameRunner.gd
@@ -1,14 +1,30 @@
 extends Node2D
 
+# Create a class to be accessible everywhere
 class_name Game
 
+# Locval variables: a player, and the comuniactionplatform, the Label
 var plyr
+var scrnText:Label
 func _init():
 	self.plyr = Player.new()
 
-func _ready():
+func _ready() -> void:
 	var msg:String = "Greetings %s" % self.plyr.playerName
 	msg += "\nYour age is: %s" % str(self.plyr.playerAge)
 	msg += "\n============================================="
-	$GUI/Control/MainTextArea.text = msg
+	self.scrnText = $GUI/Control/MainTextArea
+	self.scrnText.text = msg
+	$GUI/Control/Btn01.connect("pressed",self.hndl_btn1_press)
+	$GUI/Control/Btn02.connect("pressed",self.hndl_btn2_press)
+
+func hndl_btn1_press():
+	var msg:String = "Button 01  pressed runs"
+	scrnText.text = msg
+
+func hndl_btn2_press():
+	var msg:String = "Button 02  pressed runs"
+	scrnText.text = msg
+	
+
 	
--- TextGame01/scenes/Gui.gd
+++ TextGame01/scenes/Gui.gd
@@ -6,10 +6,10 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 
 func _on_btn_quit_pressed():
 	get_tree().quit(0)
+
+
+func _on_btn_01_pressed():
+	pass
